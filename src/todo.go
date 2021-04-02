18d6fd0333ff5b0973578f4795a33d975816a8d3
accrazed <accrazed@gmail.com>
2021-04-02 01:45:49 -0400

    added todo file for the purpose of keeping track of unimplemented objects

---
+++ src/todo.go
@@ -0,0 +1,24 @@
+// TODO: implement Cursor
+// TODO: implement Event
+// TODO: implement Forum Post
+// TODO: implement Forum Topic
+// TODO: implement GameMode
+// TODO: implement Group
+// TODO: implement KudosuHistory
+// TODO: implement MultiplayerScore
+// TODO: implement MultiplayerScores
+// TODO: implement MultiplayerScoresAround
+// TODO: implement MultiplayerScoresCursor
+// TODO: implement MultiplayerScoresSort
+// TODO: implement Notification
+// TODO: implement RankingType
+// TODO: implement Rankings
+// TODO: implement Score
+// TODO: implement Spotlight
+// TODO: implement Spotlights
+// TODO: implement Timestamp
+// TODO: implement User
+// TODO: implement UserCompact
+// TODO: implement UserGroup
+// TODO: implement UserStatistics
+// TODO: implement WikiPage
