00eaf076e08c95338334e01c4fc5173d9af83575
accrazed <accrazed@gmail.com>
2021-04-04 01:43:56 -0400

    updating test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module accrazed.com/gosu
 
 go 1.16
+
+require github.com/joho/godotenv v1.3.0 // indirect
--- src/test/test.go
+++ src/test/test.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	gosu "accrazed.com/gosu/src"
+	"github.com/joho/godotenv"
 )
 
+func getEnvVar(key string) string {
+	err := godotenv.Load("src/test/.env")
+
+	if err != nil {
+		fmt.Println("Error loading env file: %w", err)
+		return ""
+	}
+	return os.Getenv(key)
+}
+
 func main() {
-	fmt.Print("Hello, World!")
-	fmt.Printf("%d %d %d %d", gosu.Graveyard, gosu.Loved, gosu.Mania, gosu.Taiko)
+	godotenv.Load()
+	test := gosu.GetOauthToken(getEnvVar("GOSU_CLIENT_SECRET"), getEnvVar("GOSU_CLIENT_ID"))
+
+	fmt.Println(test)
+	fmt.Printf("%d %d %d %d\n%s", gosu.Graveyard, gosu.Loved, gosu.Mania, gosu.Taiko, getEnvVar("GOSU_CLIENT_SECRET"))
 }
