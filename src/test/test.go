58edfde0af6d04545bf38aca0ed22033d02cc5d3
accrazed <accrazed@gmail.com>
2021-04-16 18:25:01 -0400

    extra test cases

--- src/test/test.go
+++ src/test/test.go
@@ -9,10 +9,10 @@ import (
 )
 
 func getEnvVar(key string) string {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("src/test/.env")
 
 	if err != nil {
-		fmt.Println("Error loading env file: %w", err)
+		fmt.Println("Error loading env file:", err)
 		return ""
 	}
 	return os.Getenv(key)
@@ -22,8 +22,11 @@ func main() {
 	godotenv.Load()
 	client, _ := gosu.CreateGosuClient(getEnvVar("GOSU_CLIENT_SECRET"), getEnvVar("GOSU_CLIENT_ID"))
 
-	beatmap, _ := client.LookupBeatmap("2775178", "id")
+	// beatmap, _ := client.LookupBeatmap("2775178", "id")
+	// score, _ := client.GetUserBeatmapScore("1781697", "6725659", map[string]interface{}{})
+	// scores, _ := client.GetBeatmapScores("1781697", map[string]interface{}{})
+	beatmap, _ := client.GetBeatmap("1781697")
 
+	// fmt.Println(beatmap)
 	fmt.Println(beatmap)
-
 }
